08e79c4832070083627b273a7178d5e405da4ce5
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-11-25 21:22:27 +0300

    Add user model

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    tg_id = models.CharField(
+        'телеграм id',
+        max_length=50,
+        blank=True,
+    )
+    name = models.CharField(
+        'Имя',
+        max_length=50,
+        blank=True
+    )
+    company = models.CharField(
+        'Компания',
+        max_length=100,
+        blank=True
+    )
+    position = models.CharField(
+        'Должность',
+        max_length=100,
+        blank=True
+    )
+    STATUS_CHOICES = [
+        ('PARTICIPANT', 'Участник'),
+        ('SPEAKER', 'Спикер')
+    ]
+    status = models.CharField(
+        'Статус',
+        max_length=50,
+        choices=STATUS_CHOICES,
+        default='PARTICIPANT'
+    )
+
+    def __str__(self) -> str:
+        return f'{self.firstname} - {self.tg_id}'
+  
+    class Meta:
+        verbose_name = 'Участник',
+        verbose_name_plural = 'Участники'
