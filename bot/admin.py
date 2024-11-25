5867f470a332fdd4568b9937ed4ff90cfd2e0d0d
Макаров Степан Александрович <st.mkv03@gmail.com>
2024-11-25 22:29:47 +0300

    feat(admin): Добавил отображение моделей в админ панели

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Donate, Lecture, Program, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("tg_id", "name", "company", "position", "status")
+
+
+@admin.register(Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "start_time",
+        "end_time",
+        "speaker",
+        "status",
+    )
+
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("name", "date")
+
+
+@admin.register(Donate)
+class DonateAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "amount",
+        "donated_at",
+    )
